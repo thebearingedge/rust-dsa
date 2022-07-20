1ea9f3992385a5a0214523346a6a4dbad1e83a61
Tim Davis <7432943+thebearingedge@users.noreply.github.com>
2022-07-20 07:39:59 -0700

    remove unsafe and use vec as stack allocation instead

--- stack/src/lib.rs
+++ stack/src/lib.rs
@@ -1,61 +1,56 @@
 #[derive(Debug, PartialEq)]
 pub struct Stack<T> {
-    size: usize,
+    depth: usize,
     items: Box<[T]>,
 }
 
-impl<T: Copy + Default> Stack<T> {
-    fn new<const N: usize>() -> Self {
-        if N < 1 {
+impl<T: Default> Stack<T> {
+    pub fn new(size: usize) -> Self {
+        if size < 1 {
             panic!("Stack requires a size of at least 1 element.")
         }
         Stack {
-            size: 0,
-            items: Box::new([T::default(); N]),
+            depth: 0,
+            items: Self::alloc(size),
         }
     }
 
+    fn alloc(size: usize) -> Box<[T]> {
+        let mut items = Vec::<T>::with_capacity(size);
+        items.resize_with(size, T::default);
+        items.into_boxed_slice()
+    }
+
     pub fn peek(&self) -> Option<&T> {
-        match self.size {
+        match self.depth {
             0 => None,
-            _ => Some(&self.items[self.size - 1]),
+            _ => Some(&self.items[self.depth - 1]),
         }
     }
 
     pub fn size(&self) -> usize {
-        self.size
+        self.depth
     }
 
     pub fn push(&mut self, item: T) {
-        if self.size == self.items.len() {
-            let new_len = self.items.len() * 2;
-            let layout = std::alloc::Layout::array::<T>(new_len).unwrap();
-            unsafe {
-                let start = std::alloc::alloc(layout) as *mut T;
-                let slice = std::slice::from_raw_parts_mut(start, new_len);
-                for i in 0..self.items.len() {
-                    slice[i] = self.items[i];
-                }
-                for i in self.items.len()..slice.len() {
-                    slice[i] = T::default();
-                }
-                let _ = std::mem::replace(&mut self.items, Box::from_raw(slice));
+        if self.depth == self.items.len() {
+            let mut items = Self::alloc(self.depth * 2);
+            for i in 0..self.depth {
+                std::mem::swap(&mut self.items[i], &mut items[i]);
             }
+            self.items = items;
         }
-        self.items[self.size] = item;
-        self.size += 1;
+        self.items[self.depth] = item;
+        self.depth += 1;
     }
 
     pub fn pop(&mut self) -> Option<T> {
-        match self.size {
-            0 => None,
-            _ => {
-                self.size -= 1;
-                let item = self.items[self.size];
-                self.items[self.size] = T::default();
-                Some(item)
-            }
+        if self.depth == 0 {
+            return None;
         }
+        self.depth -= 1;
+        let item = std::mem::replace(&mut self.items[self.depth], T::default());
+        Some(item)
     }
 }
 
@@ -66,12 +61,12 @@ mod tests {
     #[test]
     #[should_panic]
     fn test_new() {
-        let _: Stack<i32> = Stack::new::<0>();
+        let _: Stack<i32> = Stack::new(0);
     }
 
     #[test]
     fn test_push() {
-        let mut stack: Stack<i32> = Stack::new::<1>();
+        let mut stack: Stack<i32> = Stack::new(1);
         stack.push(41);
         stack.push(42);
         stack.push(43);
@@ -82,19 +77,24 @@ mod tests {
 
     #[test]
     fn test_pop() {
-        let mut stack: Stack<i32> = Stack::new::<1>();
+        let mut stack: Stack<i32> = Stack::new(1);
+        assert_eq!(stack.size(), 0);
         assert_eq!(stack.pop(), None);
         stack.push(42);
+        assert_eq!(stack.size(), 1);
         assert_eq!(stack.pop(), Some(42));
         assert_eq!(stack.size(), 0);
     }
 
     #[test]
     fn test_peek() {
-        let mut stack: Stack<i32> = Stack::new::<1>();
+        let mut stack: Stack<i32> = Stack::new(1);
         assert_eq!(stack.peek(), None);
         stack.push(41);
+        assert_eq!(stack.peek(), Some(&41));
         stack.push(42);
         assert_eq!(stack.peek(), Some(&42));
+        stack.pop();
+        assert_eq!(stack.peek(), Some(&41));
     }
 }
